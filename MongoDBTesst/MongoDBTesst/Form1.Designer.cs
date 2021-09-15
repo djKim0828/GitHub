a5cbf6ea2d5a13a24df2a56dcd4d91a0145f5db8
djkim <dj.kim@enctech.co.kr>
2021-09-15 17:23:56 +0900

    테스트코드 추가

---
+++ MongoDBTesst/MongoDBTesst/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MongoDBTesst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.rcbOutput = new System.Windows.Forms.RichTextBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Location = new System.Drawing.Point(12, 60);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(162, 42);
+            this.btnSelectAll.TabIndex = 1;
+            this.btnSelectAll.Text = "Select All";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // rcbOutput
+            // 
+            this.rcbOutput.BackColor = System.Drawing.Color.Black;
+            this.rcbOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rcbOutput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rcbOutput.ForeColor = System.Drawing.SystemColors.Window;
+            this.rcbOutput.Location = new System.Drawing.Point(0, 438);
+            this.rcbOutput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rcbOutput.Name = "rcbOutput";
+            this.rcbOutput.Size = new System.Drawing.Size(784, 120);
+            this.rcbOutput.TabIndex = 39;
+            this.rcbOutput.Text = "";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitter1.Location = new System.Drawing.Point(0, 435);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(784, 3);
+            this.splitter1.TabIndex = 40;
+            this.splitter1.TabStop = false;
+            // 
+            // dgvList
+            // 
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dgvList.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dgvList.Location = new System.Drawing.Point(412, 0);
+            this.dgvList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.RowTemplate.Height = 27;
+            this.dgvList.Size = new System.Drawing.Size(372, 435);
+            this.dgvList.TabIndex = 41;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Data";
+            this.Column1.Name = "Column1";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(59, 137);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(138, 21);
+            this.txtName.TabIndex = 42;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(15, 140);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 12);
+            this.lblName.TabIndex = 43;
+            this.lblName.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Age";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(59, 173);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(138, 21);
+            this.txtAge.TabIndex = 45;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(12, 213);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(162, 42);
+            this.btnSelect.TabIndex = 46;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(12, 309);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(162, 42);
+            this.btnUpdate.TabIndex = 47;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 357);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(162, 42);
+            this.btnDelete.TabIndex = 48;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(12, 261);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(162, 42);
+            this.btnInsert.TabIndex = 49;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 558);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dgvList);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.rcbOutput);
+            this.Controls.Add(this.btnSelectAll);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.RichTextBox rcbOutput;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.DataGridView dgvList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnInsert;
+    }
+}
+
---
+++ MongoDBTesst/MongoDBTesst/Form1.cs
@@ -0,0 +1,126 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MongoDBTesst
+{
+    public partial class Form1 : Form
+    {
+        #region Fields
+
+        private MongoCollection<Customer> _table;
+        private MongoDatabase _testdb;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            Customer cust1 = new Customer { name = txtName.Text, age = txtAge.Text };
+            _table.Insert(cust1);
+
+            btnSelectAll_Click(null, null);
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            dgvList.DataSource = null;
+
+            IMongoQuery query = Query<Customer>.Where(d => d.name == txtName.Text && d.age == txtAge.Text);
+            var result = _table.Find(query).SingleOrDefault();
+            if (result != null)
+            {
+                List<Customer> result2 = new List<Customer>();
+                result2.Add(result);
+                dgvList.DataSource = result2;
+            } // else
+        }
+
+        private void btnSelectAll_Click(object sender, EventArgs e)
+        {
+            List<Customer> result2 = _table.FindAll().ToList<Customer>();
+            dgvList.DataSource = result2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connString = "mongodb://localhost";
+
+            // MongoClient 클라이언트 객체 생성
+            MongoClient cli = new MongoClient(connString);
+
+            // testdb 라는 데이타베이스 가져오기
+            // 만약 해당 DB 없으면 Collection 생성시 새로 생성함
+            _testdb = cli.GetServer().GetDatabase("testdb");
+
+            if (_testdb == null)
+            {
+                WriteLog("Failed db connect");
+            } // else
+
+            _table = _testdb.GetCollection<Customer>("testdb");
+
+            if (_table == null)
+            {
+                WriteLog("Failed db table connect");
+            } // else
+
+            WriteLog(" DB & TAble Connecting");
+
+            //IMongoQuery query = Query.EQ("name", "kildong");
+            ////IMongoQuery query = Query.EQ("_id", id);
+            //var result = _table.Find(query).SingleOrDefault();
+            //if (result != null)
+            //{
+            //    Console.WriteLine(result.ToString());
+            //}
+        }
+
+        private void WriteLog(string message)
+        {
+            rcbOutput.Invoke(new MethodInvoker(() =>
+            {
+                String nowTime = "[" + System.DateTime.Now.ToString() + "] ";
+
+                rcbOutput.AppendText(nowTime + message + "\r\n");
+                rcbOutput.SelectionStart = rcbOutput.Text.Length;
+                rcbOutput.ScrollToCaret();
+            }));
+        }
+
+        #endregion Methods
+
+        #region Classes
+
+        private class Customer
+        {
+            #region Properties
+
+            public string age { get; set; }
+
+            // 이 Id는 MongoDB Collection의 _id 컬럼과 매칭됨
+            // (예외적인 Naming Convention)
+            public ObjectId Id { get; set; }
+
+            public string name { get; set; }
+
+            #endregion Properties
+        }
+
+        #endregion Classes
+    }
+}
