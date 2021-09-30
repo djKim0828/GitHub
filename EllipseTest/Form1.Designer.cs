4312bd215384186691e49939bc2510952f2e36f2
djKim0828 <dj.kim@enctech.co.kr>
2021-09-30 17:49:46 +0900

    추가

---
+++ EllipseTest/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace EllipseTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEllipse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTotalCount = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblFOV = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblLed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtEllipse
+            // 
+            this.txtEllipse.Location = new System.Drawing.Point(87, 40);
+            this.txtEllipse.Name = "txtEllipse";
+            this.txtEllipse.Size = new System.Drawing.Size(100, 25);
+            this.txtEllipse.TabIndex = 0;
+            this.txtEllipse.Text = "640";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "원지름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "사각형";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(85, 91);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 25);
+            this.txtWidth.TabIndex = 4;
+            this.txtWidth.Text = "25";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(211, 91);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 25);
+            this.txtHeight.TabIndex = 5;
+            this.txtHeight.Text = "19";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gulim", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(185, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(85, 152);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(100, 71);
+            this.btnDraw.TabIndex = 7;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::EllipseTest.Properties.Resources.black;
+            this.pictureBox1.InitialImage = global::EllipseTest.Properties.Resources.black;
+            this.pictureBox1.Location = new System.Drawing.Point(348, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTotalCount
+            // 
+            this.lblTotalCount.AutoSize = true;
+            this.lblTotalCount.Location = new System.Drawing.Point(190, 258);
+            this.lblTotalCount.Name = "lblTotalCount";
+            this.lblTotalCount.Size = new System.Drawing.Size(15, 15);
+            this.lblTotalCount.TabIndex = 8;
+            this.lblTotalCount.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "총 사각형 갯수";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 299);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "원안의 사각형 갯수";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(191, 299);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(15, 15);
+            this.lblResult.TabIndex = 10;
+            this.lblResult.Text = "0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = global::EllipseTest.Properties.Resources.black;
+            this.pictureBox2.InitialImage = global::EllipseTest.Properties.Resources.black;
+            this.pictureBox2.Location = new System.Drawing.Point(1005, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "선택한 FOV";
+            // 
+            // lblFOV
+            // 
+            this.lblFOV.AutoSize = true;
+            this.lblFOV.Location = new System.Drawing.Point(191, 384);
+            this.lblFOV.Name = "lblFOV";
+            this.lblFOV.Size = new System.Drawing.Size(15, 15);
+            this.lblFOV.TabIndex = 13;
+            this.lblFOV.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 422);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "선택한 Led";
+            // 
+            // lblLed
+            // 
+            this.lblLed.AutoSize = true;
+            this.lblLed.Location = new System.Drawing.Point(191, 422);
+            this.lblLed.Name = "lblLed";
+            this.lblLed.Size = new System.Drawing.Size(15, 15);
+            this.lblLed.TabIndex = 15;
+            this.lblLed.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1700, 776);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblLed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFOV);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTotalCount);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtEllipse);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEllipse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label lblTotalCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblFOV;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblLed;
+    }
+}
+
---
+++ EllipseTest/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EllipseTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            this.pictureBox2.MouseMove += PictureBox2_MouseMove;
+            this.pictureBox2.MouseLeave += PictureBox2_MouseLeave;
+            this.pictureBox2.MouseClick += PictureBox2_MouseClick;
+        }
+
+        private void PictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+
+
+            int x = (e.X - _remainPoint.X) / (_detailRectWidth + _interval);
+            int y = (e.Y - _remainPoint.Y) / (_detailRectHeight + _interval);
+
+
+            lblLed.Text = x.ToString() + "_" + y.ToString();
+        }
+
+        private void PictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+
+            pictureBox2.Image = (Bitmap)pictureBox2.InitialImage.Clone();
+        }
+
+        private void PictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawDetail == false)
+            {
+                return;
+            }
+
+            this.Cursor = Cursors.Cross;
+
+            pictureBox2.Image = (Bitmap)pictureBox2.InitialImage.Clone();
+
+            int width = (_detailRectWidth + _interval) * 15;
+            int height = (_detailRectHeight + _interval) * 2;
+
+            Rectangle rectTemp = new Rectangle(e.X, e.Y, width, height);
+            Color drawColor = Color.FromArgb(255, 255, 255, 125);
+            DrawDetailRetangle(rectTemp, drawColor);
+
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = (Bitmap)pictureBox1.InitialImage.Clone();
+
+            txtEllipse.Text = (Convert.ToInt16(txtWidth.Text) * 25).ToString();
+
+
+            //보이는 원
+            Rectangle originCircle = new Rectangle(0,
+                                 0,
+                                Convert.ToInt16(txtEllipse.Text),
+                                Convert.ToInt16(txtEllipse.Text));
+
+            DrawEllipse(originCircle, true, Color.White);            
+            
+            DrawRetanglesInSideCircle();
+        }
+
+        /// <summary>
+        /// 원 그리기
+        /// </summary>
+        private void DrawEllipse(Rectangle circle, bool isFill, Color fillColor)
+        {
+            Graphics gfx = Graphics.FromImage(pictureBox1.Image);
+            
+            if (isFill == true)
+            {
+                SolidBrush iconBrush = new SolidBrush(fillColor);
+
+                gfx.FillEllipse(iconBrush, circle);
+            }
+            else
+            {
+                //Pen pen = new Pen(Color.FromArgb(255, 0, 255, 0), 1);
+                Pen pen = new Pen(fillColor, 1);
+                gfx.DrawEllipse(pen, circle);
+            }
+
+            pictureBox1.Invalidate();
+            pictureBox1.Refresh();
+        }
+
+        int _detailRectWidth = 0;
+        int _detailRectHeight = 0;
+        int _interval = 2; // 사이 간격
+        Point _remainPoint;
+
+        int _detailCol = 68;
+        int _detailRow = 86;
+
+        private void DrawDetailRects()
+        {                                  
+
+            // 간격을 포함하여 Rect를 그리기 위해 필요한 사이즈
+            int reqWidth = (pictureBox2.Width - _detailCol * _interval);
+            int reqHeight = (pictureBox2.Height - _detailRow * _interval);
+
+            // 사각형의 가로/세로 길이 구하기
+            _detailRectWidth = reqWidth / _detailCol;
+            _detailRectHeight = reqHeight / _detailRow;
+
+            // 그리고 남는 길이를 2로 나누어서 가운데에 그리도록 Remain 계산
+            int drawTotalWidth = (_detailRectWidth + _interval) * _detailCol;
+            int drawTotalHeight = (_detailRectHeight + _interval) * _detailRow;
+            _remainPoint = new Point((pictureBox2.Width - drawTotalWidth) / 2,
+                                    (pictureBox2.Height - drawTotalHeight) / 2);
+
+            int count = 0;
+            for (int i = 0; i < _detailRow; i++)
+            {
+                for (int j = 0; j < _detailCol; j++)
+                {
+                    int x = j * (_detailRectWidth + _interval) + _remainPoint.X;
+                    int y = i * (_detailRectHeight + _interval) + _remainPoint.Y;
+
+                    Rectangle rectTemp = new Rectangle(x,
+                                                       y,
+                                                       _detailRectWidth,
+                                                       _detailRectHeight);
+
+                    Color drawColor = Color.FromArgb(125, 255, 255, 255);
+                    DrawDetailRetangle(rectTemp, drawColor);
+                    count++;
+                }
+            }
+
+            pictureBox2.InitialImage = (Bitmap)pictureBox2.Image.Clone();
+
+            pictureBox2.Invalidate();
+            pictureBox2.Refresh();
+        }
+
+        private void DrawDetailRetangle(Rectangle rect, Color color)
+        {
+            Graphics gfx = Graphics.FromImage(pictureBox2.Image);
+
+            Pen pen = new Pen(color, 1);
+            gfx.DrawRectangle(pen, rect);
+        }
+
+        private void DrawRetanglesInSideCircle()
+        {
+            int rectWidth = Convert.ToInt16(txtWidth.Text);
+            int rectHeight = Convert.ToInt16(txtHeight.Text);
+
+            int circleMargin = 0; // 일단 둘레 마진
+
+            int circleDiameter = Convert.ToInt16(txtEllipse.Text) - circleMargin; // 마진을 뺀 원 크기
+            int countX = circleDiameter / rectWidth;
+            int countY = circleDiameter / rectHeight;
+
+            Point remain = new Point();
+            remain.X = circleDiameter - (countX * rectWidth);
+            remain.Y = circleDiameter - (countY * rectHeight);
+
+            // 마진 적용 원 : 마진을 구해서 센터를 기준으로 사각형이 그려지게한다.
+            Rectangle circle = new Rectangle(remain.X / 2,
+                                             remain.Y / 2,
+                                            circleDiameter - remain.X,
+                                            circleDiameter - remain.Y);
+
+            //DrawEllipse(circle, false, Color.Blue);
+
+            int drawCount = 0;
+
+            for (int i = 0; i < countY; i++)
+            {
+                for (int j = 0; j < countX; j++)
+                {
+                    Rectangle rectTemp = new Rectangle(j * rectWidth + remain.X,
+                       i * rectHeight + remain.Y,
+                       rectWidth,
+                       rectHeight);
+
+                    if (isRectangleInsideCircle(rectTemp, circle) == false)
+                    {
+                        continue; ;
+                    }
+
+                    //DrawRetangle(rectTemp);
+                    AddButton(rectTemp, i , j);
+
+                    drawCount++;
+                }
+            }
+
+            pictureBox1.Invalidate();
+            pictureBox1.Refresh();
+
+            lblTotalCount.Text = (countX * countY).ToString();
+            lblResult.Text = drawCount.ToString();
+
+        }
+
+        private void DrawRetangle(Rectangle rect)
+        {
+            Graphics gfx = Graphics.FromImage(pictureBox1.Image);
+
+            Pen pen = new Pen(Color.FromArgb(255, 255, 0, 0), 1);
+            gfx.DrawRectangle(pen, rect);            
+        }
+
+        private void AddButton(Rectangle rect, int x, int y)
+        {
+            Button newButton = new Button();
+
+            newButton.Width = rect.Width;
+            newButton.Height = rect.Height;
+            newButton.Location = rect.Location;
+            newButton.Tag = "X:" + x.ToString() + ",Y:" + y.ToString();
+            newButton.Click += NewButton_Click;
+
+            this.pictureBox1.Controls.Add(newButton);
+
+            //pictureBox1.Invalidate();
+            //pictureBox1.Refresh();
+        }
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button temp = (Button)sender;
+
+                int canvasWidth = Convert.ToInt16(txtWidth.Text) * 25;
+                int canvasHeght = Convert.ToInt16(txtHeight.Text) * 25;
+
+                pictureBox2.Size = new System.Drawing.Size(canvasWidth, canvasHeght);
+                Bitmap bm = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+                Graphics g = Graphics.FromImage(bm);
+                g.Clear(Color.Black);
+
+                pictureBox2.Image = bm;
+
+                lblFOV.Text = temp.Tag.ToString();
+
+                DrawDetailRects();
+
+                isDrawDetail = true;
+            }
+            catch (System.Exception ex)
+            {
+            	
+            }            
+        }
+
+        bool isRectangleInsideCircle(Rectangle rect, Rectangle circle)
+        {
+            bool isResult = false;
+
+            // 4점을 구한다.
+            Point leftTop = rect.Location;
+
+            Point rightTop = new Point();
+            rightTop.X = rect.Location.X + rect.Width;
+            rightTop.Y = rect.Location.Y;
+
+            Point leftButtom = new Point();
+            leftButtom.X = rect.X;
+            leftButtom.Y = rect.Y + rect.Height;
+
+            Point rightButtom = new Point();
+            rightButtom.X = rect.Location.X + rect.Width;
+            rightButtom.Y = rect.Y + rect.Height;
+
+            Point centerPoint = new Point();
+            centerPoint.X = circle.X + circle.Width / 2;
+            centerPoint.Y = circle.Y + circle.Height / 2;
+
+            int halfLength = circle.Width / 2;
+
+            if (Math.Pow((centerPoint.X - leftTop.X),2) + Math.Pow((centerPoint.Y - leftTop.Y),2) < Math.Pow(halfLength,2) == false)
+            {
+                return false;
+            }
+
+            if (Math.Pow((centerPoint.X - rightTop.X), 2) + Math.Pow((centerPoint.Y - rightTop.Y), 2) < Math.Pow(halfLength, 2) == false)
+            {
+                return false;
+            }
+
+            if (Math.Pow((centerPoint.X - leftButtom.X), 2) + Math.Pow((centerPoint.Y - leftButtom.Y), 2) < Math.Pow(halfLength, 2) == false)
+            {
+                return false;
+            }
+
+            if (Math.Pow((centerPoint.X - rightButtom.X), 2) + Math.Pow((centerPoint.Y - rightButtom.Y), 2) < Math.Pow(halfLength, 2) == false)
+            {
+                return false;
+            }
+
+            return true;
+
+
+            //double fartherstX = max(fabs(R.minX - C.cX), fabs(R.maxX - C.cX));
+            //double fartherstY = max(fabs(R.minY - C.cY), fabs(R.maxY - C.cY));
+            //return Sqr(fartherstX) + Sqr(fartherstY) < Sqr(C.Radius);
+        }
+
+
+        bool isDrawDetail = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDraw_Click(null, null);
+        }
+    }
+}
