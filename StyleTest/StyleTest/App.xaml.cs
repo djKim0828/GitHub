98e072cdc6832332949baae9fe2dc1f327a3f699
djKim0828 <dj.kim@enctech.co.kr>
2021-10-13 17:11:48 +0900

    추가

---
+++ StyleTest/StyleTest/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StyleTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            SetWPFResource(); // WPF 즉시 적용
+
+            MainWindow main = new MainWindow();
+            main.ShowDialog();
+        }
+
+        private void SetWPFResource()
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+
+            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+            {
+                Source = new Uri("pack://application:,,,/CStyle;component/resStyleCommon.xaml", UriKind.RelativeOrAbsolute)
+            });
+
+            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+            {
+                Source = new Uri("pack://application:,,,/CStyle;component/resIconPathData.xaml", UriKind.RelativeOrAbsolute)
+            });
+
+            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+            {
+                Source = new Uri("pack://application:,,,/CStyle;component/resBrushColor.xaml", UriKind.RelativeOrAbsolute)
+            });
+        }
+    }        
+}
