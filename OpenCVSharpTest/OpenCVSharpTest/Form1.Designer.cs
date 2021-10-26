44be9ed6a0f8f8074099d12d083a8e2cbbe00f5d
djKim0828 <dj.kim@enctech.co.kr>
2021-10-26 17:06:03 +0900

    Code 정리 중

---
+++ OpenCVSharpTest/OpenCVSharpTest/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace OpenCVSharpTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.cmbH = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbV = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btndivide = new System.Windows.Forms.Button();
+            this.pnlImages = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUppB = new System.Windows.Forms.TextBox();
+            this.txtUppR = new System.Windows.Forms.TextBox();
+            this.txtUppG = new System.Windows.Forms.TextBox();
+            this.txtLowR = new System.Windows.Forms.TextBox();
+            this.txtLowG = new System.Windows.Forms.TextBox();
+            this.txtLowB = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(168, 8);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(103, 43);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // cmbH
+            // 
+            this.cmbH.FormattingEnabled = true;
+            this.cmbH.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmbH.Location = new System.Drawing.Point(68, 12);
+            this.cmbH.Name = "cmbH";
+            this.cmbH.Size = new System.Drawing.Size(40, 20);
+            this.cmbH.TabIndex = 52;
+            this.cmbH.Text = "2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(110, 17);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 12);
+            this.label9.TabIndex = 54;
+            this.label9.Text = "X";
+            // 
+            // cmbV
+            // 
+            this.cmbV.FormattingEnabled = true;
+            this.cmbV.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmbV.Location = new System.Drawing.Point(127, 12);
+            this.cmbV.Name = "cmbV";
+            this.cmbV.Size = new System.Drawing.Size(42, 20);
+            this.cmbV.TabIndex = 55;
+            this.cmbV.Text = "2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(7, 17);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 12);
+            this.label10.TabIndex = 59;
+            this.label10.Text = "Divide";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.cmbV);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.cmbH);
+            this.groupBox1.Location = new System.Drawing.Point(290, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(174, 50);
+            this.groupBox1.TabIndex = 50;
+            this.groupBox1.TabStop = false;
+            // 
+            // btndivide
+            // 
+            this.btndivide.Location = new System.Drawing.Point(470, 8);
+            this.btndivide.Name = "btndivide";
+            this.btndivide.Size = new System.Drawing.Size(79, 43);
+            this.btndivide.TabIndex = 52;
+            this.btndivide.Text = "Divide";
+            this.btndivide.UseVisualStyleBackColor = true;
+            this.btndivide.Click += new System.EventHandler(this.btndivide_Click);
+            // 
+            // pnlImages
+            // 
+            this.pnlImages.BackColor = System.Drawing.Color.Transparent;
+            this.pnlImages.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlImages.Location = new System.Drawing.Point(290, 57);
+            this.pnlImages.Name = "pnlImages";
+            this.pnlImages.Size = new System.Drawing.Size(259, 296);
+            this.pnlImages.TabIndex = 53;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(569, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(259, 296);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 54;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(955, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 12);
+            this.label8.TabIndex = 68;
+            this.label8.Text = "Upper";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(955, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 12);
+            this.label5.TabIndex = 67;
+            this.label5.Text = "B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(954, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 12);
+            this.label6.TabIndex = 66;
+            this.label6.Text = "G";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(955, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 12);
+            this.label7.TabIndex = 65;
+            this.label7.Text = "R";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(834, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 12);
+            this.label4.TabIndex = 64;
+            this.label4.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(833, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 12);
+            this.label3.TabIndex = 63;
+            this.label3.Text = "G";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(834, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 12);
+            this.label2.TabIndex = 62;
+            this.label2.Text = "R";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(834, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 61;
+            this.label1.Text = "Lower";
+            // 
+            // txtUppB
+            // 
+            this.txtUppB.Location = new System.Drawing.Point(974, 135);
+            this.txtUppB.Name = "txtUppB";
+            this.txtUppB.Size = new System.Drawing.Size(78, 21);
+            this.txtUppB.TabIndex = 60;
+            this.txtUppB.Text = "0";
+            // 
+            // txtUppR
+            // 
+            this.txtUppR.Location = new System.Drawing.Point(974, 78);
+            this.txtUppR.Name = "txtUppR";
+            this.txtUppR.Size = new System.Drawing.Size(78, 21);
+            this.txtUppR.TabIndex = 58;
+            this.txtUppR.Text = "0";
+            // 
+            // txtUppG
+            // 
+            this.txtUppG.Location = new System.Drawing.Point(974, 105);
+            this.txtUppG.Name = "txtUppG";
+            this.txtUppG.Size = new System.Drawing.Size(78, 21);
+            this.txtUppG.TabIndex = 59;
+            this.txtUppG.Text = "255";
+            // 
+            // txtLowR
+            // 
+            this.txtLowR.Location = new System.Drawing.Point(853, 78);
+            this.txtLowR.Name = "txtLowR";
+            this.txtLowR.Size = new System.Drawing.Size(78, 21);
+            this.txtLowR.TabIndex = 55;
+            this.txtLowR.Text = "0";
+            // 
+            // txtLowG
+            // 
+            this.txtLowG.Location = new System.Drawing.Point(853, 105);
+            this.txtLowG.Name = "txtLowG";
+            this.txtLowG.Size = new System.Drawing.Size(78, 21);
+            this.txtLowG.TabIndex = 56;
+            this.txtLowG.Text = "127";
+            // 
+            // txtLowB
+            // 
+            this.txtLowB.Location = new System.Drawing.Point(853, 132);
+            this.txtLowB.Name = "txtLowB";
+            this.txtLowB.Size = new System.Drawing.Size(78, 21);
+            this.txtLowB.TabIndex = 57;
+            this.txtLowB.Text = "0";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(836, 171);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(103, 43);
+            this.btnSearch.TabIndex = 69;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 365);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUppB);
+            this.Controls.Add(this.txtUppR);
+            this.Controls.Add(this.txtUppG);
+            this.Controls.Add(this.txtLowR);
+            this.Controls.Add(this.txtLowG);
+            this.Controls.Add(this.txtLowB);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pnlImages);
+            this.Controls.Add(this.btndivide);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ComboBox cmbH;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbV;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btndivide;
+        private System.Windows.Forms.Panel pnlImages;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUppB;
+        private System.Windows.Forms.TextBox txtUppR;
+        private System.Windows.Forms.TextBox txtUppG;
+        private System.Windows.Forms.TextBox txtLowR;
+        private System.Windows.Forms.TextBox txtLowG;
+        private System.Windows.Forms.TextBox txtLowB;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ OpenCVSharpTest/OpenCVSharpTest/Form1.cs
@@ -0,0 +1,185 @@
+﻿using OpenCvSharp.CPlusPlus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenCVSharpTest
+{
+    public partial class Form1 : Form
+    {
+        private Mat _srcImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                LoadImage(dlg.FileName);
+            } // else
+        }
+
+        private void LoadImage(string imageFileName)
+        {
+            if (_srcImage != null)
+            {
+                _srcImage.Release();
+                _srcImage = null;
+
+                GC.Collect();
+                System.Threading.Thread.Sleep(100);
+            }
+
+            _srcImage = Cv2.ImRead(imageFileName);
+            pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_srcImage);
+        }
+
+        private void btndivide_Click(object sender, EventArgs e)
+        {
+            RunDivide(Convert.ToInt16(cmbH.Text), Convert.ToInt16(cmbV.Text));
+        }
+
+        private void RunDivide(short h, short v)
+        {
+            pnlImages.Controls.Clear();
+
+            int pbWidth = _srcImage.Width / h;
+            int pbHeight = _srcImage.Height / v;
+
+            int pnlWidth = 0;
+            int pnlHeight = 0;
+
+            int lineThickness = 2;
+
+            for (int i = 0; i < v; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.Name = "pb" + (h * i + j).ToString();
+                    pb.Width = pbWidth;
+                    pb.Height = pbHeight;
+                    pb.Left = pbWidth * j + (lineThickness * j);
+                    pb.Top = pbHeight * i + (lineThickness * i);                    
+
+                    Rect rect = new Rect(pbWidth * j,
+                                         pbHeight * i,
+                                         pbWidth,
+                                        pbHeight);
+
+                    pb.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_srcImage.SubMat(rect));
+                    pnlImages.Controls.Add(pb);
+
+                    pnlWidth += pbWidth + lineThickness;
+                    pnlHeight += pbHeight + lineThickness;
+                }
+            }
+
+            pnlImages.Width = pnlWidth / v;
+            pnlImages.Height = pnlHeight / h;
+           
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Mat yellow = new Mat();
+            Mat dst = _srcImage.Clone();
+
+            OpenCvSharp.CPlusPlus.Point[][] contours;
+
+            int lowR = Convert.ToInt16(txtLowR.Text);
+            int lowG = Convert.ToInt16(txtLowG.Text);
+            int lowB = Convert.ToInt16(txtLowB.Text);
+
+            int uppR = Convert.ToInt16(txtUppR.Text);
+            int uppG = Convert.ToInt16(txtUppG.Text);
+            int uppB = Convert.ToInt16(txtUppB.Text);
+
+            Cv2.InRange(_srcImage, new Scalar(lowB, lowG, lowR),
+                        new Scalar(uppB, uppG, uppR), yellow);
+
+            SearchContours0(yellow, out contours);
+
+            if (contours.Length < 1)
+            {
+                return;
+            } // else
+
+            List<OpenCvSharp.CPlusPlus.Point[]> new_contours = new List<OpenCvSharp.CPlusPlus.Point[]>();
+            foreach (OpenCvSharp.CPlusPlus.Point[] p in contours)
+            {
+                double length = Cv2.ArcLength(p, true);
+                if (length > 100)
+                {
+                    new_contours.Add(p);
+                }
+            }
+
+            Cv2.DrawContours(dst,
+                            new_contours,
+                            -1,
+                            new Scalar(0, 0, 255),   // 라인색
+                            1,   // 라인 굵기
+                            OpenCvSharp.LineType.AntiAlias,
+                            null,
+                            1);
+
+            pictureBox2.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+        }
+
+        private void SearchContours0(Mat yellow, out OpenCvSharp.CPlusPlus.Point[][] contours)
+        {
+
+            HierarchyIndex[] hierarchy;
+
+            Cv2.FindContours(yellow, out contours, out hierarchy, OpenCvSharp.ContourRetrieval.External,
+                OpenCvSharp.ContourChain.ApproxTC89KCOS);
+
+            if (contours.Length > 1)
+            {
+                return;
+            } // else            
+
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Cv2.FindContours(yellow, out contours, out hierarchy, OpenCvSharp.ContourRetrieval.CComp,
+                            OpenCvSharp.ContourChain.ApproxTC89KCOS);
+                        break;
+
+                    case 1:
+                        Cv2.FindContours(yellow, out contours, out hierarchy, OpenCvSharp.ContourRetrieval.List,
+                            OpenCvSharp.ContourChain.ApproxTC89KCOS);
+                        break;
+
+                    case 2:
+                        Cv2.FindContours(yellow, out contours, out hierarchy, OpenCvSharp.ContourRetrieval.FloodFill,
+                            OpenCvSharp.ContourChain.ApproxTC89KCOS);
+                        break;
+
+                    default:
+                        Cv2.FindContours(yellow, out contours, out hierarchy, OpenCvSharp.ContourRetrieval.Tree,
+                            OpenCvSharp.ContourChain.ApproxTC89KCOS);
+                        break;
+                }
+
+                if (contours.Length > 1)
+                {                    
+                    break;
+                } // else
+            }
+        }
+    }
+}
